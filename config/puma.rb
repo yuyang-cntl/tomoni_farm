8465b828cf7084cc7b93d14f028e82d5f2f80f57
Ubuntu <ubuntu@ip-172-31-35-41.ap-northeast-1.compute.internal>
2025-09-18 15:01:40 +0000

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -40,10 +40,11 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 rails_root = Dir.pwd
+bind "unix://#{rails_root}/tmp/sockets/puma.sock"
+
 # 本番環境のみデーモン起動
-if Rails.env.production?
+if ENV['RAILS_ENV'] == 'production'
   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
