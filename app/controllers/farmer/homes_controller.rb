62e20188d8613a0e2e4fcee71966c3ca318a20a3
Ubuntu <ubuntu@ip-172-31-35-41.ap-northeast-1.compute.internal>
2025-09-20 14:45:24 +0000

    farmer/homes/controller修正

--- app/controllers/farmer/homes_controller.rb
+++ app/controllers/farmer/homes_controller.rb
@@ -1,8 +1,14 @@
 class Farmer::HomesController < ApplicationController
   before_action :authenticate_farmer!
+
   def top
     @farmer = current_farmer
-    @customer = @farmer.followers.find_by(params[:customer_id])
-    @orders = @customer.orders.page(params[:page]).per(10)
+    @customer = @farmer.followers.find_by(id: params[:customer_id])
+
+    @orders = if @customer.present?
+      @customer.orders.page(params[:page]).per(10)
+    else
+      Order.none.page(params[:page]).per(10)
+    end
   end
 end
