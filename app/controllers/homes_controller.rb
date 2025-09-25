97002c6f2c1d54afd7ed982d07cb45970c62ebba
EC2 Default User <ec2-user@ip-172-31-39-217.ap-northeast-1.compute.internal>
2025-09-25 12:01:42 +0000

    本番反映：appのみ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   skip_before_action :redirect_guest_to_root, only: [:top]
   def top
+    @farmer = Farmer.find_by(email: 'farmer@example.com')
   end
 
   def about
