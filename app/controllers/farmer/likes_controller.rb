ae3dfe60ade66a201c91769c564d7fd8365d8d23
EC2 Default User <ec2-user@ip-172-31-39-217.ap-northeast-1.compute.internal>
2025-09-28 07:17:30 +0000

    本番反映：appとpublicのみ

--- app/controllers/farmer/likes_controller.rb
+++ app/controllers/farmer/likes_controller.rb
@@ -2,6 +2,6 @@ class Farmer::LikesController < ApplicationController
   def index
     @diary = current_farmer.diaries.find(params[:diary_id])
     @post = @diary.posts.find(params[:post_id])
-    @likes = @post.likes.includes(:customer)
+    @likes = @post.likes.includes(customer: {image_attachment: :blob})
   end
 end
