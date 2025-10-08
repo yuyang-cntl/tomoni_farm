d3eca5f433553830d474521ef2c6acbf9b3303dc
EC2 Default User <ec2-user@ip-172-31-39-217.ap-northeast-1.compute.internal>
2025-10-08 11:04:02 +0000

    本番反映：app

--- app/controllers/public/farmers_controller.rb
+++ app/controllers/public/farmers_controller.rb
@@ -2,11 +2,7 @@ class Public::FarmersController < ApplicationController
   before_action :authenticate_customer!
   
   def index
-    if current_customer.farmer.present?
-      @farmers = current_customer.farmers.includes(images_attachments: :blob).where(is_active: true)
-      else
-        @farmers = []
-      end
+    @farmers = current_customer.followed_farmers.includes(profile_image_attachment: :blob).where(is_active: true)
   end
 
 end
