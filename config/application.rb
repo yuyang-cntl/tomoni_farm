7ca1b28606e6068101d0ffed78c2d68e15ac3182
EC2 Default User <ec2-user@ip-172-31-39-217.ap-northeast-1.compute.internal>
2025-10-30 06:09:59 +0000

    本番反映：config

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,8 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require 'dotenv'
-Dotenv.load('.env') if defined?(Dotenv)
+require 'dotenv-rails' if defined?(Dotenv)
+Dotenv::Railtie.load if defined?(Dotenv)
 
 module TomoniFarm
   class Application < Rails::Application
