a296c58f8aa26192ff96fc9ab415cb3a992c06e2
EC2 Default User <ec2-user@ip-172-31-39-217.ap-northeast-1.compute.internal>
2025-11-08 05:05:24 +0000

    本番反映：app

--- app/controllers/farmer/broadcasts_controller.rb
+++ app/controllers/farmer/broadcasts_controller.rb
@@ -5,6 +5,11 @@ class Farmer::BroadcastsController < ApplicationController
   end
 
   def create
+    if params[:subject].blank? || params[:body].blank?
+      flash[:alert] = "件名と本文は必須です"
+      redirect_to new_farmer_broadcast_path and return
+    end
+    
     service = FarmerBroadcastService.new(
       current_farmer,
       subject: params[:subject],
