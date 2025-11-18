88a9c517e0b8053d87ece56a20043117233d68ae
EC2 Default User <ec2-user@ip-172-31-39-217.ap-northeast-1.compute.internal>
2025-11-18 15:43:25 +0000

    本番反映：app

--- app/controllers/farmer/statistics_controller.rb
+++ app/controllers/farmer/statistics_controller.rb
@@ -1,18 +1,18 @@
 class Farmer::StatisticsController < ApplicationController
   def index
     # 商品ごとの月別売上
-    @stacked_sales = Item.all.map do |item|
+    @stacked_sales = Item.where(farmer_id: current_farmer.id).map do |item|
       monthly_data = Order.where(item_id: item.id)
                           .group_by_month(:created_at, format: "%Y年%m月", time_zone: false)
                           .sum(:grand_total)
       {name: item.name, data: monthly_data}
     end
+    item_ids = Item.where(farmer_id: current_farmer.id).pluck(:id)
     @monthly_sales = Order.group_by_month(:created_at, format: "%Y年%m月", time_zone: false).sum(:grand_total)
     @item_sales = Order.joins(:item).group("name").sum(:grand_total)
     @yearly_sales = Order.group_by_year(:created_at, format: "%Y年", time_zone: false).sum(:grand_total)
     # 投稿に対するいいね数
-    @stacked_likes = Post.joins(:diary).map do |post|
-      next unless post.diary.present?
+    @stacked_likes = Post.joins(:diary).where(farmer_id: current_farmer.id).map do |post|
       like_count = post.likes.count
       post_date = post.created_at.strftime("%Y年%m月%d日")
       {
@@ -21,6 +21,6 @@ class Farmer::StatisticsController < ApplicationController
         date: post_date,
         link: farmer_diary_post_path(post.diary, post)
       }
-    end.compact
+    end
   end
 end
